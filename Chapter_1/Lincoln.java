2fad168230d67543c872fe0ee2f1da6d8ca3ca61
Genji <genjinakano@gmail.com>
2019-01-11 20:54:59 -0800

    Adding Chapter_1

---
+++ Chapter_1/Lincoln.java
@@ -0,0 +1,15 @@
+// Demonstrate basic structure of a Java Application
+
+public class Lincoln
+{
+   // ------
+   // Prints a Presential Quote
+   // -----
+   public static void main(String[] args)
+   {
+      System.out.println("A quote by Abraham Lincoln");
+      
+      System.out.println("Whatever you are, be a good one");
+      
+   }
+}
